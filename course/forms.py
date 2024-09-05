4ec072518ad90a75ff8d303587eb48842aa62999
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-09-05 17:16:46 +0300

    made changes

--- course/forms.py
+++ course/forms.py
@@ -4,4 +4,4 @@ from .models import Course
 class CourseRegistationForm(forms.ModelForm):
     class Meta:
         model=Course
-        fields=["course_name","course_id","course_code","course_TA","course_term","course_description","course_department","course_syllabus","course_instructor","course_duration"]
+        fields=["course_name","course_id","course_code","course_TA","course_term","course_description","course_department","course_syllabus","course_instructor"]
